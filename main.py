50a1bb08a4a78e5a472ad5282088a62e5dbf0cd5
Artur <arturrodriguesmota@hotmail.com>
2024-12-27 15:12:58 -0300

    Adiciona funcao para adicionar capacidade nas arestas

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ class TransporteFluxo:
         self.V = cidades # cria o número de cidades (os nós do meu grafo)
         self.grafo = [[0] * cidades for _ in range(cidades)] # cria uma matriz de adjascência para o grafo
     
+    def adicionaRotas(self, origem, destino, capacidade):
+        self.grafo[origem][destino] = capacidade # adiciona a capacidade c(e) na aresta (u, v)
+      
+    
     
