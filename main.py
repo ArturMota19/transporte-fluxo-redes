f22f02359639bb719e54c463bf99d6cbea04b2bb
Artur <arturrodriguesmota@hotmail.com>
2024-12-27 15:21:01 -0300

    Adicionando funcao de busca em profundidade

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ class TransporteFluxo:
     def adicionaRotas(self, origem, destino, capacidade):
         self.grafo[origem][destino] = capacidade # adiciona a capacidade c(e) na aresta (u, v)
       
+    def _dfs(self, origem, destino, pais):
+        visitado = [False] * self.V # cria um vetor de visitados com o tamanho do número de cidades
+        stack = [origem] # cria uma pilha com a origem
+        visitado[origem] = True # marca a origem como visitada
+
+        while stack: 
+            u = stack.pop() # remove o último elemento da pilha
+
+            for v in range(self.V):
+                if not visitado[v] and self.grafo[u][v] > 0: # se ainda não foi visitado e a capacidade da aresta é maior que 0
+                    stack.append(v) # adiciona o vértice na pilha
+                    visitado[v] = True # marca o vértice como visitado
+                    pais[v] = u # marca o pai do vértice como u
+                    if v == destino:
+                        return True
+        return False 
     
     
